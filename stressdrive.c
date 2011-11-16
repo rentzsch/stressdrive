15a18cad5109b256e61b32395be9edbd97d17515
rentzsch <jwr.git@redshed.net>
2011-11-16 15:20:20 -0600

    Now verifies written data.

--- stressdrive.c
+++ stressdrive.c
@@ -1,3 +1,8 @@
+// stressdrive.c 1.0
+//   Copyright (c) 2011 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+//   Some rights reserved: http://opensource.org/licenses/MIT
+//   https://github.com/rentzsch/stressdrive
+
 #include <stdio.h>
 #include <assert.h>
 #include <sys/stat.h>
@@ -9,6 +14,7 @@
 #include <errno.h>
 #include <sys/time.h>
 #include <sys/disk.h>
+#include <strings.h>
 
 int main(int argc, const char *argv[]) {
     if (argc != 2) {
@@ -19,7 +25,7 @@ int main(int argc, const char *argv[]) {
     const char *drivePath = argv[1];
     int fd = open(drivePath, O_RDWR);
     if (fd == -1) {
-        perror("open(2) failed");
+        perror("open() failed");
         exit(EXIT_FAILURE);
     }
     
@@ -38,13 +44,13 @@ int main(int argc, const char *argv[]) {
     printf("blockCount: %llu\n", blockCount);
     
     SHA_CTX shaContext;
-	unsigned char shaDigest[SHA_DIGEST_LENGTH];
-	SHA1_Init(&shaContext);
-    
     struct timeval start;
     gettimeofday(&start, NULL);
-    
     uint8_t *block = malloc(blockSize);
+    
+    printf("writing random data to %s\n", drivePath);
+    SHA1_Init(&shaContext);
+    uint8_t writtenShaDigest[SHA_DIGEST_LENGTH];
     for (uint64_t blockIndex = 0; blockIndex < blockCount; blockIndex++) {
         int err = RAND_pseudo_bytes(block, blockSize);
         assert(err == 0 || err == 1);
@@ -62,15 +68,63 @@ int main(int argc, const char *argv[]) {
         timersub(&now, &start, &delta);
         if (delta.tv_sec >= 1) {
             gettimeofday(&start, NULL);
-            printf("\r%.0f%% (block %lld of %lld)",
+            printf("\rwriting %.0f%% (block %lld of %lld)",
                    ((double)blockIndex / (double)blockCount) * 100.0,
                    blockIndex,
                    blockCount);
             fflush(stdout);
         }
     }
-    free(block);
+    
     printf("\n");
+    SHA1_Final(writtenShaDigest, &shaContext);
+    for (int shaByteIndex = 0; shaByteIndex < SHA_DIGEST_LENGTH; shaByteIndex++) {
+        printf("%02x", writtenShaDigest[shaByteIndex]);
+    }
+    printf(" <= SHA-1 of written data\n", drivePath);
+    
+    if (lseek(fd, 0LL, SEEK_SET) != 0LL) {
+        perror("lseek() failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("verifying written data\n");
+    SHA1_Init(&shaContext);
+    uint8_t readShaDigest[SHA_DIGEST_LENGTH];
+    for (uint64_t blockIndex = 0; blockIndex < blockCount; blockIndex++) {
+        if (read(fd, block, blockSize) == -1) {
+            perror("read() failed");
+            exit(EXIT_FAILURE);
+        }
+        SHA1_Update(&shaContext, block, blockSize);
+        
+        struct timeval now, delta;
+        gettimeofday(&now, NULL);
+        timersub(&now, &start, &delta);
+        if (delta.tv_sec >= 1) {
+            gettimeofday(&start, NULL);
+            printf("\rreading %.0f%% (block %lld of %lld)",
+                   ((double)blockIndex / (double)blockCount) * 100.0,
+                   blockIndex,
+                   blockCount);
+            fflush(stdout);
+        }
+    }
+    
+    printf("\n");
+    SHA1_Final(readShaDigest, &shaContext);
+    for (int shaByteIndex = 0; shaByteIndex < SHA_DIGEST_LENGTH; shaByteIndex++) {
+        printf("%02x", readShaDigest[shaByteIndex]);
+    }
+    printf(" <= SHA-1 of read data\n", drivePath);
+    
+    if (bcmp(writtenShaDigest, readShaDigest, SHA_DIGEST_LENGTH) == 0) {
+        printf("SUCCESS\n");
+    } else {
+        printf("FAILURE\n");
+    }
+    
+    free(block);
     
     /*
      This ends up kicking the entire device off the bus, which is too much:
@@ -84,12 +138,5 @@ int main(int argc, const char *argv[]) {
      */
     
     close(fd);
-    SHA1_Final(shaDigest, &shaContext);
-    printf("SHA-1: ");
-    for (int shaByteIndex = 0; shaByteIndex < SHA_DIGEST_LENGTH; shaByteIndex++) {
-        printf("%02x", shaDigest[shaByteIndex]);
-    }
-    printf("  %s (SHA-1 of written data)\n", drivePath);
-    
     return 0;
 }
