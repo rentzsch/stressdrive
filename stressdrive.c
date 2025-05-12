2148c862be033f6fb9de1c565e9c29b1dedcd54c
Ivan Kuchin <tadump+git@gmail.com>
2025-05-13 01:12:17 +0200

    generate/write/hash and read/hash in separate threads
    Use 2 buffers to generate, write and hash, as writing and hashing can be
    done in parallel on same buffer

--- stressdrive.c
+++ stressdrive.c
@@ -11,6 +11,8 @@
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <openssl/sha.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -154,6 +156,176 @@ void DIGEST_Print(unsigned char *digest, const char *name) {
     printf(" <= hash digest of %s data\n", name);
 }
 
+#define BUFFER_COUNT 2
+
+typedef enum {
+    Generate,
+    Process,
+    Read,
+    Hash,
+} Action;
+
+typedef struct {
+    uint8_t *data;
+    Action action;
+    bool written, hashed;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} Buffer;
+
+typedef struct {
+    Buffer buffers[BUFFER_COUNT];
+    uint64_t blockCount;
+    uint16_t bufferBlocks;
+    uint32_t blockSize;
+    PROGRESS_CTX *progress;
+    EVP_CIPHER_CTX *cipher;
+    unsigned char *cipherInput;
+    int fd;
+} Shared;
+
+void *generator_thread(void *arg) {
+    Shared *shared = (Shared *)arg;
+
+    uint64_t blockCount = shared->blockCount;
+    uint16_t bufferBlocks = shared->bufferBlocks;
+    uint32_t blockSize = shared->blockSize;
+    PROGRESS_CTX *progress = shared->progress;
+    EVP_CIPHER_CTX *cipher = shared->cipher;
+    unsigned char *cipherInput = shared->cipherInput;
+
+    int bufferIndex = 0;
+    Buffer *buffer = &shared->buffers[0];
+    for (uint64_t blockIndex = 0; blockIndex < blockCount;
+         blockIndex += bufferBlocks) {
+        if (blockIndex)
+            PROGRESS_Update(progress, blockIndex, blockSize);
+
+        uint32_t size =
+            (uint32_t)MIN(bufferBlocks, blockCount - blockIndex) * blockSize;
+
+        if (buffer->action != Generate) {
+            pthread_mutex_lock(&buffer->mutex);
+            while (buffer->action != Generate) {
+                pthread_cond_wait(&buffer->cond, &buffer->mutex);
+            }
+            pthread_mutex_unlock(&buffer->mutex);
+        }
+
+        int outSize;
+        if (!EVP_EncryptUpdate(cipher, buffer->data, &outSize, cipherInput,
+                               size)) {
+            fprintf(stderr, "EVP_EncryptUpdate() failed\n");
+            exit(EXIT_CALL_FAILED);
+        }
+        if (outSize != size) {
+            fprintf(stderr,
+                    "EVP_EncryptUpdate() returned %d instead of %u bytes\n",
+                    outSize, size);
+            exit(EXIT_CALL_FAILED);
+        }
+
+        pthread_mutex_lock(&buffer->mutex);
+        buffer->action = Process;
+        pthread_cond_broadcast(&buffer->cond);
+        pthread_mutex_unlock(&buffer->mutex);
+
+        bufferIndex = (bufferIndex + 1) % BUFFER_COUNT;
+        buffer = &shared->buffers[bufferIndex];
+    }
+
+    return NULL;
+}
+
+void *writer_thread(void *arg) {
+    Shared *shared = (Shared *)arg;
+
+    uint64_t blockCount = shared->blockCount;
+    uint16_t bufferBlocks = shared->bufferBlocks;
+    uint32_t blockSize = shared->blockSize;
+    int fd = shared->fd;
+
+    int bufferIndex = 0;
+    Buffer *buffer = &shared->buffers[0];
+    for (uint64_t blockIndex = 0; blockIndex < blockCount;
+         blockIndex += bufferBlocks) {
+        uint32_t size =
+            (uint32_t)MIN(bufferBlocks, blockCount - blockIndex) * blockSize;
+
+        if (buffer->action != Process || buffer->written) {
+            pthread_mutex_lock(&buffer->mutex);
+            while (buffer->action != Process || buffer->written) {
+                pthread_cond_wait(&buffer->cond, &buffer->mutex);
+            }
+            pthread_mutex_unlock(&buffer->mutex);
+        }
+
+        if (write(fd, buffer->data, size) != size) {
+            perror("write() failed");
+            exit(EXIT_CALL_FAILED);
+        }
+
+        pthread_mutex_lock(&buffer->mutex);
+        if (buffer->hashed) {
+            buffer->action = Generate;
+            buffer->hashed = false;
+        } else {
+            buffer->written = true;
+        }
+        pthread_cond_broadcast(&buffer->cond);
+        pthread_mutex_unlock(&buffer->mutex);
+
+        bufferIndex = (bufferIndex + 1) % BUFFER_COUNT;
+        buffer = &shared->buffers[bufferIndex];
+    }
+
+    return NULL;
+}
+
+void *reader_thread(void *arg) {
+    Shared *shared = (Shared *)arg;
+
+    uint64_t blockCount = shared->blockCount;
+    uint16_t bufferBlocks = shared->bufferBlocks;
+    uint32_t blockSize = shared->blockSize;
+    PROGRESS_CTX *progress = shared->progress;
+    int fd = shared->fd;
+
+    int bufferIndex = 0;
+    Buffer *buffer = &shared->buffers[0];
+    for (uint64_t blockIndex = 0; blockIndex < blockCount;
+         blockIndex += bufferBlocks) {
+        if (blockIndex)
+            PROGRESS_Update(progress, blockIndex, blockSize);
+
+        uint32_t size =
+            (uint32_t)MIN(bufferBlocks, blockCount - blockIndex) * blockSize;
+
+        if (buffer->action != Read) {
+            pthread_mutex_lock(&buffer->mutex);
+            while (buffer->action != Read) {
+                pthread_cond_wait(&buffer->cond, &buffer->mutex);
+            }
+            pthread_mutex_unlock(&buffer->mutex);
+        }
+
+        if (read(fd, buffer->data, size) == -1) {
+            perror("read() failed");
+            exit(EXIT_CALL_FAILED);
+        }
+
+        pthread_mutex_lock(&buffer->mutex);
+        buffer->action = Hash;
+        pthread_cond_broadcast(&buffer->cond);
+        pthread_mutex_unlock(&buffer->mutex);
+
+        bufferIndex = (bufferIndex + 1) % BUFFER_COUNT;
+        buffer = &shared->buffers[bufferIndex];
+    }
+
+    return NULL;
+}
+
 int main(int argc, const char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "stressdrive v1.4\n");
@@ -203,12 +375,6 @@ int main(int argc, const char *argv[]) {
 
     uint32_t bufferSize = MAX(blockSize, 8 * 1024 * 1024);
     printf("buffer size: %u\n", bufferSize);
-
-    uint8_t *buffer = malloc(bufferSize);
-    if (buffer == NULL) {
-        perror("malloc() failed");
-        exit(EXIT_CALL_FAILED);
-    }
 
     uint16_t bufferBlocks = bufferSize / blockSize;
     uint32_t checkFrequency = 1024 * 1024 * 1024 / blockSize;
@@ -264,34 +430,72 @@ int main(int argc, const char *argv[]) {
     unsigned char *cipherInput = malloc(bufferSize);
     memset(cipherInput, 0, bufferSize);
 
+    Shared *shared = &(Shared){
+        .blockCount = blockCount,
+        .bufferBlocks = bufferBlocks,
+        .blockSize = blockSize,
+        .progress = &progress,
+        .cipher = cipher,
+        .cipherInput = cipherInput,
+        .fd = fd,
+    };
+
+    for (int i = 0; i < BUFFER_COUNT; i++) {
+        if ((shared->buffers[i].data = malloc(bufferSize)) == NULL) {
+            perror("malloc() failed");
+            exit(EXIT_CALL_FAILED);
+        }
+        shared->buffers[i].action = Generate;
+        if (pthread_mutex_init(&shared->buffers[i].mutex, NULL) != 0) {
+            perror("pthread_mutex_init() failed");
+            exit(EXIT_CALL_FAILED);
+        }
+        if (pthread_cond_init(&shared->buffers[i].cond, NULL) != 0) {
+            perror("pthread_cond_init() failed");
+            exit(EXIT_CALL_FAILED);
+        }
+    }
+
+    pthread_t t_reader, t_generator, t_writer;
+    int bufferIndex;
+    Buffer *buffer;
+
     printf("writing random data to %s\n", drivePath);
     DIGEST_Init(digestContext);
     PROGRESS_Init(&progress, blockCount, "writing");
+
+    pthread_create(&t_generator, NULL, generator_thread, shared);
+    pthread_create(&t_writer, NULL, writer_thread, shared);
+
+    bufferIndex = 0;
+    buffer = &shared->buffers[0];
     for (uint64_t blockIndex = 0; blockIndex < blockCount;
          blockIndex += bufferBlocks) {
-        if (blockIndex)
-            PROGRESS_Update(&progress, blockIndex, blockSize);
-
         uint32_t size =
             (uint32_t)MIN(bufferBlocks, blockCount - blockIndex) * blockSize;
 
-        int outSize;
-        if (!EVP_EncryptUpdate(cipher, buffer, &outSize, cipherInput, size)) {
-            fprintf(stderr, "EVP_EncryptUpdate() failed\n");
-            exit(EXIT_CALL_FAILED);
-        }
-        if (outSize != size) {
-            fprintf(stderr,
-                    "EVP_EncryptUpdate() returned %d instead of %u bytes\n",
-                    outSize, size);
-            exit(EXIT_CALL_FAILED);
+        if (buffer->action != Process || buffer->hashed) {
+            pthread_mutex_lock(&buffer->mutex);
+            while (buffer->action != Process || buffer->hashed) {
+                pthread_cond_wait(&buffer->cond, &buffer->mutex);
+            }
+            pthread_mutex_unlock(&buffer->mutex);
         }
 
-        if (write(fd, buffer, size) != size) {
-            perror("write() failed");
-            exit(EXIT_CALL_FAILED);
+        DIGEST_Update(digestContext, buffer->data, size);
+
+        pthread_mutex_lock(&buffer->mutex);
+        if (buffer->written) {
+            buffer->action = Generate;
+            buffer->written = false;
+        } else {
+            buffer->hashed = true;
         }
-        DIGEST_Update(digestContext, buffer, size);
+        pthread_cond_broadcast(&buffer->cond);
+        pthread_mutex_unlock(&buffer->mutex);
+
+        bufferIndex = (bufferIndex + 1) % BUFFER_COUNT;
+        buffer = &shared->buffers[bufferIndex];
 
         if ((blockIndex + bufferBlocks) % checkFrequency == 0) {
             uint64_t checkIndex = blockIndex / checkFrequency;
@@ -300,6 +504,10 @@ int main(int argc, const char *argv[]) {
             DIGEST_Init(digestContext);
         }
     }
+
+    pthread_join(t_writer, NULL);
+    pthread_join(t_generator, NULL);
+
     PROGRESS_Finish(&progress, blockSize);
     EVP_CIPHER_CTX_free(cipher);
 
@@ -315,22 +523,40 @@ int main(int argc, const char *argv[]) {
     int exitCode = EXIT_SUCCESS;
     uint8_t readHashDigest[HASH_DIGEST_LENGTH];
 
+    for (int i = 0; i < BUFFER_COUNT; i++) {
+        shared->buffers[i].action = Read;
+    }
+
     printf("verifying written data\n");
     DIGEST_Init(digestContext);
     PROGRESS_Init(&progress, blockCount, "reading");
+
+    pthread_create(&t_reader, NULL, reader_thread, shared);
+
+    bufferIndex = 0;
+    buffer = &shared->buffers[0];
     for (uint64_t blockIndex = 0; blockIndex < blockCount;
          blockIndex += bufferBlocks) {
-        if (blockIndex)
-            PROGRESS_Update(&progress, blockIndex, blockSize);
-
         uint32_t size =
             (uint32_t)MIN(bufferBlocks, blockCount - blockIndex) * blockSize;
 
-        if (read(fd, buffer, size) == -1) {
-            perror("read() failed");
-            exit(EXIT_CALL_FAILED);
+        if (buffer->action != Hash) {
+            pthread_mutex_lock(&buffer->mutex);
+            while (buffer->action != Hash) {
+                pthread_cond_wait(&buffer->cond, &buffer->mutex);
+            }
+            pthread_mutex_unlock(&buffer->mutex);
         }
-        DIGEST_Update(digestContext, buffer, size);
+
+        DIGEST_Update(digestContext, buffer->data, size);
+
+        pthread_mutex_lock(&buffer->mutex);
+        buffer->action = Read;
+        pthread_cond_broadcast(&buffer->cond);
+        pthread_mutex_unlock(&buffer->mutex);
+
+        bufferIndex = (bufferIndex + 1) % BUFFER_COUNT;
+        buffer = &shared->buffers[bufferIndex];
 
         if ((blockIndex + bufferBlocks) % checkFrequency == 0) {
             uint64_t checkIndex = blockIndex / checkFrequency;
@@ -346,6 +572,9 @@ int main(int argc, const char *argv[]) {
             }
         }
     }
+
+    pthread_join(t_reader, NULL);
+
     PROGRESS_Finish(&progress, blockSize);
     DIGEST_Final(digestContext, readHashDigest);
     DIGEST_Print(readHashDigest, "read");
@@ -369,8 +598,13 @@ int main(int argc, const char *argv[]) {
     }
 #endif
 
+    for (int i = 0; i < BUFFER_COUNT; i++) {
+        free(shared->buffers[i].data);
+        pthread_mutex_destroy(&shared->buffers[i].mutex);
+        pthread_cond_destroy(&shared->buffers[i].cond);
+    }
+
     free(checkDigests);
-    free(buffer);
     close(fd);
 
     return exitCode;
